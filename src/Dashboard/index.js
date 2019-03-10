4b60f4148b14cb9d8a5d744d4f2a113bfa5ea70d
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-03-09 20:01:10 -0600

    added functionality to open a new log form and post a new log

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -9,13 +9,20 @@ class Dashboard extends React.Component {
 		this.state = {
 			logs: [],
 			account: {
+				_id: null,
 				username: '',
 				email: '',
 				firstName: '',
 				lastName: ''
 			},
+			newLog: {
+				content: '',
+				date: '',
+				thumbnail: ''
+			}
 			accountSettingsClassName: 'display-none',
-			dashClassName: 'logs-dash'
+			dashClassName: 'logs-dash',
+			newLogClassName: 'display-none'
 		}
 	}
 	componentDidMount = async () => {
@@ -33,6 +40,56 @@ class Dashboard extends React.Component {
 			logs: response.data
 		})
 	}
+	setSettingsClassDisplayNone = () => {
+		this.setState({
+			accountSettingsClassName: "display-none",
+			dashClassName: "logs-dash"
+		})
+	}
+	handleChangeAccount = (e) => {
+		this.setState({
+			account: {
+				...this.state.account,
+				[e.target.name]: e.target.value
+			}
+		});
+	}
+	handleChangeLog = (e) => {
+		this.setState({
+			newLog: {
+				...this.state.newLog,
+				[e.target.name]: e.target.value
+			}
+		})
+	}
+	showNewLogForm = () => {
+		this.setSTate({
+			newLogClassName: "logs-new",
+			dashClassName: "display-none"
+		})
+	}
+	postNewLog = async (e) => {
+		e.preventDefault();
+		const newLog = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
+			'/api/v1/chaseDay/logs/new_log', {
+				method: 'POST',
+				credentials: 'include',
+				body: JSON.stringify(this.state.newLog),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		)
+		if (!newLog.ok) {
+			throw Error(newLog.statusText)
+		}
+		const response = await newLog.json();
+		this.setState({
+			logs: [this.state.saved, response.data.log],
+			newLogClassName: "display-none",
+			dashClassName: "logs-dash"
+		})
+	}
 	showAccountSettings = async () => {
 		const account = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
 			'/api/v1/chaseDay/user/account_settings', {
@@ -46,6 +103,7 @@ class Dashboard extends React.Component {
 		const response = await account.json();
 		this.setState({
 			account: {
+				_id: response.data.id,
 				username: response.data.username,
 				email: response.data.email,
 				firstName: response.data.firstName,
@@ -55,16 +113,31 @@ class Dashboard extends React.Component {
 			dashClassName: "display-none"
 		})
 	}
-	setSettingsClassDisplayNone = () => {
-		this.setState({
-			accountSettingsClassName: "display-none",
-			dashClassName: "logs-dash"
+	updateAccount = async (e) => {
+		e.preventDefault();
+		const updateAccount = await fetch(process.env.REACT_APP_CLIENT_APP_URI +
+			'/api/v1/chaseDay/user/update_settings/' + this.state.account._id, {
+				method: 'PUT',
+				credentials: 'include',
+				body: JSON.stringify(this.state.account),
+				headers: {
+					'Content-Type': 'application/json'
+				}
 		})
-	}
-	handleChange = (e) => {
+		if (!updateAccount.ok) {
+			throw Error(updateAccount.statusText)
+		}
+		const response = await updateAccount.json();
 		this.setState({
-			[e.target.name]: e.target.value
-		});
+			account: {
+				_id: null,
+				username: '',
+				email: '',
+				firstName: '',
+				lastName: ''
+			}
+		})
+		this.setSettingsClassDisplayNone();
 	}
 	render() {
 		console.log(this.state)
@@ -79,6 +152,7 @@ class Dashboard extends React.Component {
 			<div className="dash-wrap">
 				<div className={this.state.dashClassName}>
 					<h1>this is your dashboard</h1>
+					<button onClick={this.showNewLogForm}>Write a New Log</button>
 					<div className="logs">
 						<ul>
 							{logs}
@@ -89,7 +163,7 @@ class Dashboard extends React.Component {
 				<div className={this.state.accountSettingsClassName}>
 					<Settings account={this.state.account} 
 					closeSettings={this.setSettingsClassDisplayNone} 
-					handleChange={this.handleChange}
+					handleChange={this.handleChangeAccount}
 					updateAccount={this.updateAccount}/>
 				</div>
 			</div>
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -11,28 +11,28 @@ const Settings = (props) => {
 						<input type='text' name='username' 
 						value={props.account.username} 
 						placeholder='Username' 
-						onChange={props.handleChange} />
+						onChange={props.handleChangeAccount} />
 					</label>
 					<br />
 					<label>Email: <br />
 						<input type='email' name='email' 
 						value={props.account.email} 
 						placeholder='Email' 
-						onChange={props.handleChange} />
+						onChange={props.handleChangeAccount} />
 					</label>
 					<br />
 					<label>First Name: <br />
 						<input type='text' name='firstName' 
 						value={props.account.firstName} 
 						placeholder='First Name' 
-						onChange={props.handleChange} />
+						onChange={props.handleChangeAccount} />
 					</label>
 					<br />
 					<label>Last Name: <br />
 						<input type='text' name='lastName' 
 						value={props.account.lastName} 
 						placeholder='Last Name' 
-						onChange={props.handleChange} />
+						onChange={props.handleChangeAccount} />
 					</label>
 					<br />
 					<input type='submit' />	
