51634803fe5509f7dea43e8db48da3dec9fbad3d
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-03-09 22:00:24 -0600

    built in functionality for rating posts, not fully working yet

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -3,6 +3,7 @@ import '../App.css';
 import './index.css';
 import Settings from '../Settings';
 import LogForm from '../LogForm';
+import Profile from '../Profile';
 
 class Dashboard extends React.Component {
 	constructor() {
@@ -21,25 +22,32 @@ class Dashboard extends React.Component {
 				date: '',
 				thumbnail: ''
 			},
-			accountSettingsClassName: 'display-none',
 			dashClassName: 'logs-dash',
-			newLogClassName: 'display-none'
+			accountSettingsClassName: 'display-none',
+			accountProfileClassName: 'display-none',
+			newLogClassName: 'display-none',
+			rating: ''
 		}
 	}
 	componentDidMount = async () => {
-		const logs = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
-			'/api/v1/chaseDay/logs/dashboard', {
-				method: 'GET',
-				credentials: 'include'
+		try {
+			const logs = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
+				'/api/v1/chaseDay/logs/dashboard', {
+					method: 'GET',
+					credentials: 'include'
+				}
+			)
+			if (!logs.ok) {
+				throw Error(logs.statusText)
 			}
-		)
-		if (!logs.ok) {
-			throw Error(logs.statusText)
+			const response = await logs.json();
+			this.setState({
+				logs: response.data
+			})
+		} catch (err) {
+			console.log(err)
+			return(err)
 		}
-		const response = await logs.json();
-		this.setState({
-			logs: response.data
-		})
 	}
 	setSettingsClassDisplayNone = () => {
 		this.setState({
@@ -76,83 +84,164 @@ class Dashboard extends React.Component {
 		})
 	}
 	postNewLog = async (e) => {
-		e.preventDefault();
-		const newLog = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
-			'/api/v1/chaseDay/logs/new_log', {
-				method: 'POST',
-				credentials: 'include',
-				body: JSON.stringify(this.state.newLog),
-				headers: {
-					'Content-Type': 'application/json'
+		try {
+			e.preventDefault();
+			const newLog = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
+				'/api/v1/chaseDay/logs/new_log', {
+					method: 'POST',
+					credentials: 'include',
+					body: JSON.stringify(this.state.newLog),
+					headers: {
+						'Content-Type': 'application/json'
+					}
 				}
+			)
+			if (!newLog.ok) {
+				throw Error(newLog.statusText)
 			}
-		)
-		if (!newLog.ok) {
-			throw Error(newLog.statusText)
+			const response = await newLog.json();
+			this.setState({
+				logs: [this.state.saved, response.data.log]
+			})
+			this.setNewLogClassDisplayNone();
+		} catch (err) {
+			console.log(err)
+			return(err)
 		}
-		const response = await newLog.json();
-		this.setState({
-			logs: [this.state.saved, response.data.log]
-		})
-		this.setNewLogClassDisplayNone();
 	}
 	showAccountSettings = async () => {
-		const account = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
-			'/api/v1/chaseDay/user/account_settings', {
-				method: 'GET',
-				credentials: 'include'
+		try {
+			const account = await fetch(process.env.REACT_APP_CLIENT_APP_URI +
+				'/api/v1/chaseDay/user/account_settings', {
+					method: 'GET',
+					credentials: 'include'
+				}
+			)
+			if (!account.ok) {
+				throw Error(account.statusText)
 			}
-		)
-		if (!account.ok) {
-			throw Error(account.statusText)
+			const response = await account.json();
+			this.setState({
+				account: {
+					_id: response.data.id,
+					username: response.data.username,
+					email: response.data.email,
+					firstName: response.data.firstName,
+					lastName: response.data.lastName
+				},
+				accountSettingsClassName: "account-settings",
+				dashClassName: "display-none"
+			})
+		} catch (err) {
+			console.log(err)
+			return(err)
 		}
-		const response = await account.json();
-		this.setState({
-			account: {
-				_id: response.data.id,
-				username: response.data.username,
-				email: response.data.email,
-				firstName: response.data.firstName,
-				lastName: response.data.lastName
-			},
-			accountSettingsClassName: "account-settings",
-			dashClassName: "display-none"
-		})
 	}
 	updateAccount = async (e) => {
 		e.preventDefault();
-		const updateAccount = await fetch(process.env.REACT_APP_CLIENT_APP_URI +
-			'/api/v1/chaseDay/user/update_settings/' + this.state.account._id, {
+		try {
+			const updateAccount = await fetch(process.env.REACT_APP_CLIENT_APP_URI +
+					'/api/v1/chaseDay/user/update_settings/' +
+					this.state.account._id, {
 				method: 'PUT',
 				credentials: 'include',
 				body: JSON.stringify(this.state.account),
 				headers: {
 					'Content-Type': 'application/json'
 				}
-		})
-		if (!updateAccount.ok) {
-			throw Error(updateAccount.statusText)
-		}
-		const response = await updateAccount.json();
-		this.setState({
-			account: {
-				_id: null,
-				username: '',
-				email: '',
-				firstName: '',
-				lastName: ''
+			})
+			if (!updateAccount.ok) {
+				throw Error(updateAccount.statusText)
 			}
+			await updateAccount.json();
+			this.setState({
+				account: {
+					_id: null,
+					username: '',
+					email: '',
+					firstName: '',
+					lastName: ''
+				}
+			})
+			this.setSettingsClassDisplayNone();
+		} catch (err) {
+			console.log(err)
+			return(err)
+		}
+	}
+	// stopped by cors, or perhaps the body isn't translating?
+	rateUp = async (id, e) => {
+		e.preventDefault()
+		this.setState({
+			rating: 'up'
 		})
-		this.setSettingsClassDisplayNone();
+		console.log(this.state)
+		try {
+			const rating = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
+				'/api/v1/chaseDay/ratings/rate/' + id, {
+					method: 'POST',
+					credentials: 'include',
+					body: JSON.stringify(this.state.rating),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+			)
+			console.log('request sent')
+			if (!rating.ok) {
+				throw Error(rating.statusText)
+			}
+			await rating.json();
+			this.setState({
+				rating: ''
+			})
+		} catch (err) {
+			console.log(err)
+			return(err)
+		}
+	}
+	// ""
+	rateDown = async (id, e) => {
+		e.preventDefault()
+		this.setState({
+			rating: 'down'
+		})
+		console.log(this.state)
+		try {
+			const rating = await fetch(process.env.REACT_APP_CLIENT_APP_URI + 
+				'/api/v1/chaseDay/ratings/rate/' + id, {
+					method: 'POST',
+					credentials: 'include',
+					body: JSON.stringify(this.state.rating),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+			)
+			console.log('request sent')
+			if (!rating.ok) {
+				throw Error(rating.statusText)
+			}
+			await rating.json();
+			this.setState({
+				rating: ''
+			})
+		} catch (err) {
+			console.log(err)
+			return(err)
+		}
 	}
 	render() {
 		console.log(this.state)
 		const logs = this.state.logs.map((log, i) => {
-			return <li key={i}>
+			return <li key={log.id}>
 				{log.author} <br />
 				{log.createdAt.toString()} <br />
-				{log.thumbnail} <br />
+				<img src={log.thumbnail} alt={log.author}/> <br />
 				{log.content} <br /><br />
+				<button onClick={this.rateUp.bind(null, log.id)}>Rate Up</button>
+				<button onClick={this.rateDown.bind(null, log.id)}>Rate Down</button>
+				<br /><br />
 			</li>
 		})
 		return (
